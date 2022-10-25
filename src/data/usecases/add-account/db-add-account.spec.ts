72bd20c1e05ee68bc5718769e14ecaecb9bb9bd3
NeoticoZ <heriksaraujodias@gmail.com>
2022-10-25 19:02:12 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -111,3 +111,21 @@ it('Should throw if AddAccountRepository throws', async () => {
 
   await expect(promise).rejects.toThrow()
 })
+
+it('Should return an account on success', async () => {
+  const { sut } = makeSut()
+  const accountData = {
+    name: 'valid_name',
+    email: 'valid_email@email',
+    password: 'valid_password'
+  }
+
+  const account = await sut.add(accountData)
+
+  expect(account).toEqual({
+    id: 'valid_id',
+    name: 'valid_name',
+    email: 'valid_email@email',
+    password: 'hashed_password'
+  })
+})
